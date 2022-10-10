28d3bcf9f23fc27b6b335fa09e255d0025a0b47c
peterdudfield <peter.dudfield@hotmail.com>
2022-10-10 18:03:55 +0100

    add sip analysis to delta

--- scripts/calculate_curtailment.py
+++ scripts/calculate_curtailment.py
@@ -1,9 +1,11 @@
 import sys
 from pathlib import Path
 
+import pandas as pd
 import plotly.express as px
 
 from lib.db_utils import DbRepository
+from lib.sbp_utils import get_sbp_data
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -26,13 +28,14 @@ def run(db: DbRepository, start_time, end_time):
     curtailment_dfs = []
     units = df_boal.index.unique()
 
-    for unit in units:
+    for i, unit in enumerate(units):
         df_curtailment_unit = analyze_one_unit(df_boal_unit=df_boal.loc[unit], df_fpn_unit=df_fpn.loc[unit])
 
         curtailment_in_mwh = calculate_curtailment_in_mwh(df_curtailment_unit)
         generation_in_mwh = calculate_notified_generation_in_mwh(df_curtailment_unit)
 
         print(f"Curtailment for {unit} is {curtailment_in_mwh:.2f} MWh. Generation was {generation_in_mwh:.2f} MWh")
+        print(f'Done {i} out of {len(units)}')
 
         curtailment_dfs.append(df_curtailment_unit)
 
@@ -51,7 +54,32 @@ def run(db: DbRepository, start_time, end_time):
 
     fig.show()
 
+    print('Getting SIP prices')
+    sip = pd.DataFrame(get_sbp_data())
+    delta = df_[['delta', 'Time']]
+    delta.set_index('Time',inplace=True)
+    delta = delta.resample('30min').mean()
+    sip_and_delta = sip.join(delta, how='inner')
+
+    # plot time series
+    fig = px.line(sip_and_delta)
+    fig.update_layout(
+        yaxis=dict(title="MW or £/MWH"),
+        xaxis=dict(title="Time"),
+        title=dict(text="SIP and Curtailment"),
+    )
+    fig.show()
+
+    # plot scatter
+    fig = px.scatter(x=sip_and_delta['sip'],y=sip_and_delta['delta'])
+    fig.update_layout(
+        yaxis=dict(title="SIP [£/MWh]"),
+        xaxis=dict(title="Curtailment MW"),
+        title=dict(text="Scatter: SIP and Curtailment"),
+    )
+    fig.show()
+
 
 if __name__ == "__main__":
     db = DbRepository(BASE_DIR / "scripts/phys_data.db")
-    run(db, start_time="2022-01-01", end_time="2022-04-01")
+    run(db, start_time="2022-01-01", end_time="2022-02-01")
