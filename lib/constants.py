f3013de4fbe75254ef694d6c80566e59322b3348
Archy de Berker <archy@carbonchain.io>
2022-11-27 14:25:16 +0000

    Add a script to push data to postgres instance

--- lib/constants.py
+++ lib/constants.py
@@ -1,6 +1,11 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
 DATA_DIR = BASE_DIR / "data"
 SAVE_DIR = DATA_DIR / "PHYBM/raw"
 SQL_DIR = BASE_DIR / "sql"
+
+DB_IP = os.environ.get("DB_IP")
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
