dad691fec9c81e250a289b00f010b03130576b96
peterdudfield <peter.dudfield@hotmail.com>
2022-12-20 09:50:28 +0000

    option to only have one calendar, then the 3 graphs update automatically

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 
 from lib.curtailment import MINUTES_TO_HOURS
@@ -25,14 +26,16 @@ df['level_after_boal_mwh'] = df['level_after_boal_mw'] * 0.5
 
 
 MIN_DATE = pd.to_datetime("2022-01-01")
-MAX_DATE = pd.to_datetime("2023-01-01")
+MAX_DATE = pd.to_datetime("2024-01-01")
+st.session_state.today_date = pd.to_datetime("today").date()
 INITIAL_END_DATE = pd.to_datetime("2023-01-01")
 
 st.title("UK Wind Curtailment")
 
-start_date = st.date_input("Start Time", min_value=MIN_DATE, max_value=MAX_DATE, value=MIN_DATE)
-end_date = st.date_input("End Time", min_value=MIN_DATE, max_value=MAX_DATE, value=INITIAL_END_DATE)
-filtered_df = filter_data(df.copy(), start_date, end_date).copy()
+select_date = st.date_input("Select Date", min_value=MIN_DATE, max_value=MAX_DATE, value=st.session_state.today_date)
+month_and_year = pd.to_datetime(select_date).month_name() + ' ' + str(pd.to_datetime(select_date).year)
+
+filtered_df = filter_data(df.copy(), MIN_DATE, MAX_DATE).copy()
 filtered_df["month_and_year"] = filtered_df["time"].dt.month_name() + ' ' + filtered_df["time"].dt.year.astype(str)
 
 total_curtailment = filtered_df["delta_mw"].sum() * MINUTES_TO_HOURS
@@ -49,40 +52,33 @@ year_df = year_df.sort_values(by=['month_idx'])
 
 yearly_curtailment_twh = year_df["delta_mw"].sum() / 10 ** 6 * 0.5
 yearly_curtailment_mgbp = year_df["cost_gbp"].sum() / 10 ** 6
-st.write(f"Wind Curtailment {yearly_curtailment_twh:.2f} TWh: £ {yearly_curtailment_mgbp:.2f} M")
+st.write(f"Total Wind Curtailment {yearly_curtailment_twh:.2f} TWh: £ {yearly_curtailment_mgbp:.2f} M")
 
 # monthly plot
 fig = make_time_series_plot(year_df.copy(), title=f"Wind Curtailment for 2022", mw_or_mwh='mwh')
 st.plotly_chart(fig)
 
-# drop down box for months
-option_month = st.selectbox("Select a month", list(year_df["month_and_year"]))
-
 # get the monthly data
-monthly_df = filtered_df[filtered_df["month_and_year"] == option_month]
+monthly_df = filtered_df[filtered_df["month_and_year"] == month_and_year]
 monthly_df["time"] = monthly_df["time"].dt.date
 monthly_df = monthly_df.groupby("time").sum()
 monthly_df["time"] = monthly_df.index
 
 monthly_curtailment_gwh = monthly_df["delta_mw"].sum() / 10 ** 3 * 0.5
 monthly_curtailment_kgbp = monthly_df["cost_gbp"].sum() / 10 ** 6
-st.write(f"Wind Curtailment {monthly_curtailment_gwh:.2f} GWh: £ {monthly_curtailment_kgbp:.2f} M")
+st.write(f"Wind Curtailment for {month_and_year} {monthly_curtailment_gwh:.2f} GWh: £ {monthly_curtailment_kgbp:.2f} M")
 
 # daily plot plot
-fig = make_time_series_plot(monthly_df.copy(), title=f"Wind Curtailment for {option_month}", mw_or_mwh='mwh')
+fig = make_time_series_plot(monthly_df.copy(), title=f"Wind Curtailment for {month_and_year}", mw_or_mwh='mwh')
 st.plotly_chart(fig)
 
-# day drop droplet
-option_day = st.selectbox("Select a date", list(monthly_df["time"]))
-# get the day data
-
-daily_df = filtered_df[filtered_df["time"].dt.date == option_day]
+daily_df = filtered_df[filtered_df["time"].dt.date == select_date]
 daily_df = daily_df.groupby("time").sum()
 daily_df["time"] = daily_df.index
 
 daily_curtailment_gwh = daily_df["delta_mw"].sum() / 10 ** 3 * 0.5
 daily_curtailment_kgbp = daily_df["cost_gbp"].sum() / 10 ** 6
-st.write(f"Wind Curtailment {daily_curtailment_gwh:.2f} GWh: £ {daily_curtailment_kgbp:.2f} M")
+st.write(f"Wind Curtailment for {select_date} {daily_curtailment_gwh:.2f} GWh: £ {daily_curtailment_kgbp:.2f} M")
 
-fig = make_time_series_plot(daily_df.copy(), title=f"Wind Curtailment for {option_day}")
+fig = make_time_series_plot(daily_df.copy(), title=f"Wind Curtailment for {select_date}")
 st.plotly_chart(fig)
