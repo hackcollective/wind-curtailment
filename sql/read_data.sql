fa24960b12b3ded44f1027afebff7f2070535621
Archy de Berker <archy@carbonchain.io>
2022-12-27 14:22:53 +0000

    Add turnup cost to SQL query

--- sql/read_data.sql
+++ sql/read_data.sql
@@ -1,10 +1,12 @@
-select level_fpn              as level_fpn_mw,
-       level_after_boal       as level_after_boal_mw,
+select level_fpn                         as level_fpn_mw,
+       level_after_boal                  as level_after_boal_mw,
        delta_mw,
        level_fpn_mwh,
-       level_fpn * 0.5        as level_fpn_mwh,
-       level_after_boal * 0.5 as level_after_boal_mwh,
-       system_buy_price
+       level_fpn * 0.5                   as level_fpn_mwh,
+       level_after_boal * 0.5            as level_after_boal_mwh,
+       system_buy_price,
+       cost_gbp,
+       system_buy_price * delta_mw * 0.5 as turnup_cost_gbp
 from curtailment c
          join sbp s on c.time = s.time
 where time BETWEEN :start_time AND :end_time
