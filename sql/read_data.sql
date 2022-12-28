fc66a23e9532dc9f68461f1b71873fb9eb95c66b
Archy de Berker <archy@carbonchain.io>
2022-12-28 12:02:39 +0000

    Fix read data query and mount SQL dir in docker compose

--- sql/read_data.sql
+++ sql/read_data.sql
@@ -2,10 +2,10 @@
 --> 2. Manipulates some columns which used to be manipulated in pandas (faster in SQL)
 --> 3. start and end time are parameters which are interpolated by the SQL engine
 
-select level_fpn                         as level_fpn_mw,
+select c.time                            as time,
+       level_fpn                         as level_fpn_mw,
        level_after_boal                  as level_after_boal_mw,
        delta_mw,
-       level_fpn_mwh,
        level_fpn * 0.5                   as level_fpn_mwh,
        level_after_boal * 0.5            as level_after_boal_mwh,
        system_buy_price,
@@ -13,5 +13,6 @@ select level_fpn                         as level_fpn_mw,
        system_buy_price * delta_mw * 0.5 as turnup_cost_gbp
 from curtailment c
          join sbp s on c.time = s.time
-where time BETWEEN :start_time AND :end_time
-order by time
+where c.time BETWEEN CAST(%(start_time)s as TIMESTAMP)
+    AND CAST(%(end_time)s as TIMESTAMP)
+order by c.time
