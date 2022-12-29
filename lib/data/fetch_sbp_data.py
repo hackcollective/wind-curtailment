9087cca6ee0bbea7a8759caa1cb1a5541cbe4adb
peterdudfield <peter.dudfield@hotmail.com>
2022-12-29 19:43:26 +0000

    fix bug with timezones

--- lib/data/fetch_sbp_data.py
+++ lib/data/fetch_sbp_data.py
@@ -6,8 +6,11 @@ def call_sbp_api(start_date: str, end_date: str):
     data_df = client.get_DERSYSDATA(start_date=start_date, end_date=end_date)
 
     # filter on start and end date, as we sometimes get more
-    start_date = start_date.tz_localize(tz='Europe/London')
-    end_date = end_date.tz_localize(tz='Europe/London')
+    if start_date.tzinfo is None:
+        start_date = start_date.tz_localize(tz='Europe/London')
+    if end_date.tzinfo is None:
+        end_date = end_date.tz_localize(tz='Europe/London')
+        
     data_df = data_df[data_df['local_datetime'] >= start_date]
     data_df = data_df[data_df['local_datetime'] < end_date]
 
--- tests/data/test_fetch_sbp.py
+++ tests/data/test_fetch_sbp.py
@@ -13,3 +13,13 @@ def test_get_sbp():
 
     df.to_csv("sbp.csv")
 
+
+def test_get_sbp_with_timezone():
+    start = pd.Timestamp("2022-01-01T00:00:00+00:00")
+    end = pd.Timestamp("2022-02-01 00:00:00+00:00")
+    _ = call_sbp_api(
+        start_date=start,
+        end_date=end,
+    )
+
+
