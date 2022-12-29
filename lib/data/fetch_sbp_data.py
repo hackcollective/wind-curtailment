5c3fac0f69d38705a16c38a46f71f2ba28e40616
peterdudfield <peter.dudfield@hotmail.com>
2022-12-29 19:48:59 +0000

    add logging

--- lib/data/fetch_sbp_data.py
+++ lib/data/fetch_sbp_data.py
@@ -1,17 +1,23 @@
 from lib.data.utils import client
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def call_sbp_api(start_date: str, end_date: str):
+
+    logger.info(f"Getting SIP from {start_date} to {end_date}")
 
     data_df = client.get_DERSYSDATA(start_date=start_date, end_date=end_date)
 
     # filter on start and end date, as we sometimes get more
     if start_date.tzinfo is None:
-        start_date = start_date.tz_localize(tz='Europe/London')
+        start_date = start_date.tz_localize(tz="Europe/London")
     if end_date.tzinfo is None:
-        end_date = end_date.tz_localize(tz='Europe/London')
-        
-    data_df = data_df[data_df['local_datetime'] >= start_date]
-    data_df = data_df[data_df['local_datetime'] < end_date]
+        end_date = end_date.tz_localize(tz="Europe/London")
+
+    data_df = data_df[data_df["local_datetime"] >= start_date]
+    data_df = data_df[data_df["local_datetime"] < end_date]
 
     return data_df
