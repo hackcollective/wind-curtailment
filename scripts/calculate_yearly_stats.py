f4af96590ba865984956e1996363ee4b552d575b
Archy de Berker <archy@carbonchain.io>
2023-01-02 21:50:31 +0000

    Add notebook and script for calculating yearly totals

--- scripts/calculate_yearly_stats.py
+++ scripts/calculate_yearly_stats.py
@@ -15,20 +15,37 @@ RENEWABLES = ["BIOMASS", "WIND", "NPSHYD", "OTHER"]
 
 
 def fetch_data_elexon_insights():
+    """Fetching from
+    https://developer.data.elexon.co.uk/api-details#api=prod-insol-insights-api&operation=get-generation-outturn-summary
+
+    The frequency of returned data varies randomly with the amount requested, amazingly. 2 day chunks seems to yield
+    30 minute resolution i.e. SP.
+    """
     start_time = pd.to_datetime("2022-01-01")
-    today =  pd.to_datetime("2022-12-31")
-    end_time = start_time + datetime.timedelta(days=1)
+    today = pd.to_datetime("2022-12-31")
+    end_time = start_time + datetime.timedelta(days=2)
     data = []
-    # while end_time < today:
-    r = requests.get(
-        "https://data.elexon.co.uk/bmrs/api/v1/generation/outturn/summary",
-        params=dict(startTime=start_time, endTime=end_time, format="json"),
-    )
-    data.append(r.json())
-    start_time = end_time
-    end_time += +datetime.timedelta(days=1)
+    while end_time < today:
+        r = requests.get(
+            "https://data.elexon.co.uk/bmrs/api/v1/generation/outturn/summary",
+            params=dict(startTime=start_time, endTime=end_time, format="json"),
+        )
+        data.extend(r.json())
+        start_time = end_time
+        end_time += +datetime.timedelta(days=1)
+        logger.info(end_time)
 
     return data
+
+def format_data_as_df(data):
+    dfs = []
+    for day in data:
+        row = pd.json_normalize(day["data"]).set_index("fuelType").T
+        row.index = [day["startTime"]]
+        dfs.append(row)
+
+    return pd.concat(dfs)
+
 
 def fetch_data_b1620(start="2022-01-01", end="2022-01-01"):
     logger.info("Fetching")
@@ -36,15 +53,23 @@ def fetch_data_b1620(start="2022-01-01", end="2022-01-01"):
     return data
 
 
-def analyze_data(data: pd.DataFrame):
+def analyze_data(df: pd.DataFrame):
+    df.index = pd.to_datetime(df.index)
+    df_hourly = df.resample("H").mean()
+    df_total_generation = df_hourly.sum(axis=1)
+    total_generation_twh = df_total_generation.sum() / 1e6
+    for fuel, item in df_hourly.sum().iteritems():
+        print(f"{fuel} {item / 1e6} Twh")
 
-    data["generation"] = data["generation"].astype(float)
-    renewable_generation = data[data["fuelType"].isin(RENEWABLES)]["generation"].sum()
-
-    total_generation = data["generation"].sum()
-    start, end = data["startTime"].min(), data["startTime"].max()
-
-    return renewable_generation, total_generation, (start, end)
+    total_renewables_twh = df_hourly[RENEWABLES].sum().sum() / 1e6
+    pct_renewables = total_renewables_twh / total_generation_twh * 100
+    print(f"{pct_renewables=}")
+    wind_twh = df_hourly.sum()["WIND"] / 1e6
+    pct_wind_of_total = wind_twh / total_generation_twh * 100
+    print(f"{pct_wind_of_total=}")
+    curtailment_twh = 3.6  # from app
+    pct_curtailment = curtailment_twh / wind_twh * 100
+    print(f"{pct_curtailment=}")
 
 
 if __name__ == "__main__":
@@ -54,15 +79,9 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG)
 
-    data = fetch_data_elexon_insights()
-    print(data)
+    # data = fetch_data_elexon_insights()
+    # df = format_data_as_df(data)
+    # df.to_csv("elexon_yearly_generation.csv")
 
-    dfs = [pd.concat((pd.json_normalize(d["data"]), df["startTime"]), axis=1) for d in data]
-    df = pd.concat(dfs)
-
-    df.to_csv("yearly_generation.csv")
-    renewables, total, time_period = analyze_data(df)
-    print(f"{renewables*1e-3=:,f} gwh")
-    print(f"{total*1e-3=:,f} gwh")
-    print(f"{renewables/total*100=:.1f}%")
-    print(time_period)
+    df = pd.read_csv("elexon_yearly_generation.csv", index_col=0)
+    analyze_data(df)
