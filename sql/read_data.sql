6fa0fea007923a0f37e0ead5044ea1cc4ddd3141
peterdudfield <peter.dudfield@hotmail.com>
2023-01-06 09:11:50 +0000

    remove sbp from query, as sbp as some gaps in it

--- sql/read_data.sql
+++ sql/read_data.sql
@@ -9,12 +9,12 @@ select c.time                            as time,
        delta_mw,
        level_fpn * 0.5                   as level_fpn_mwh,
        level_after_boal * 0.5            as level_after_boal_mwh,
-       system_buy_price,
+-->        system_buy_price,
        cost_gbp,
 -->    system_buy_price * delta_mw * 0.5 as turnup_cost_gbp
        200 * delta_mw * 0.5 as turnup_cost_gbp
 from curtailment c
-         join sbp s on c.time = s.time
+         --> join sbp s on c.time = s.time
 where c.time BETWEEN CAST(%(start_time)s as TIMESTAMP)
     AND CAST(%(end_time)s as TIMESTAMP)
 order by c.time
