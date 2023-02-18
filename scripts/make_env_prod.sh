eb6a8abf8a9b0af28a4804df9d96534f178f3bf5
peterdudfield <peter.dudfield@hotmail.com>
2023-02-18 18:49:16 +0000

    tidy

--- scripts/make_env_prod.sh
+++ scripts/make_env_prod.sh
@@ -1,4 +1,5 @@
 # this scripts makes the file "env/.env.prod" and uses DB_PASSWORD from environmental variables
+echo "Making env/.env.prod file"
 echo "DB_IP=34.88.31.153" >> env/.env.prod
 echo "DB_USERNAME=postgres" >> env/.env.prod
 echo "DB_PASSWORD=${DB_PASSWORD}" >> env/.env.prod
