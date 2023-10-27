7c3830a368e0296488b3b598f224594280419363
peterdudfield <peter.dudfield@hotmail.com>
2023-10-27 15:03:12 +0100

    update tests

--- tests/data/test_main.py
+++ tests/data/test_main.py
@@ -10,13 +10,13 @@ def test_fetch_and_load_data():
     assert len(df) == 2
     assert df["delta_mw"].mean() > 4000
     assert df["delta_mw"].mean() < 5000
-    assert df["level_fpn"].mean() > 12000
-    assert df["level_fpn"].mean() < 13000
+    assert df["level_fpn"].mean() > 13000
+    assert df["level_fpn"].mean() < 14000
     assert df["level_after_boal"].mean() > 8000
     assert df["level_after_boal"].mean() < 9000
 
     assert df["cost_gbp"].mean() >= 206000
-    assert df["cost_gbp"].mean() <= 207000
+    assert df["cost_gbp"].mean() <= 208000
 
     assert pd.to_datetime(df['time'][0]) == pd.Timestamp("2022-06-11 05:00:00+01:00")
 
@@ -30,12 +30,12 @@ def test_fetch_and_load_data_winter():
     assert len(df) == 2
     assert df["delta_mw"].mean() > 4000
     assert df["delta_mw"].mean() < 5000
-    assert df["level_fpn"].mean() > 15000
-    assert df["level_fpn"].mean() < 16000
-    assert df["level_after_boal"].mean() > 10000
-    assert df["level_after_boal"].mean() < 11000
+    assert df["level_fpn"].mean() > 16000
+    assert df["level_fpn"].mean() < 17000
+    assert df["level_after_boal"].mean() > 11000
+    assert df["level_after_boal"].mean() < 12000
 
     assert df["cost_gbp"].mean() >= 206000
-    assert df["cost_gbp"].mean() <= 207000
+    assert df["cost_gbp"].mean() <= 208000
 
     assert pd.to_datetime(df['time'][0]) == pd.Timestamp("2022-11-11 05:00:00+00:00")
